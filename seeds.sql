cabeb1b72c241111d25dd274216a8269eb4212f7
Anna Olt <anna.olt16@gmail.com>
2020-04-09 17:01:58 -0400

    added seeds file

---
+++ seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO questions
+    ("left", "right")
+VALUES
+    ("Only be to use a fork (no spoon)", "Only be able to use a spoon (no fork)"),
+    ("Wear a wedding dress/tuxedo every single day", "wear a bathing suit every single day"),
+    ("Have all dogs try to attack you when they see you", "All birds try to attack you when they see you"),
+    ("Have to fart loudly every time you have a serious conversation", "have to burp after every kiss"),
+    ("Be able to teleport anywhere", "Be able to read minds"),
+    ("Have one nipple", "Have two belly buttons"),
+    ("Have a concersation with your past self", "Have a conversation with your future self"),
+    ("Fight 100 duck sized horses", "Fight one horse sized duck"),
+    ("Swallow broken glass", "swallow rusty nails")
+;
+
+
+    
