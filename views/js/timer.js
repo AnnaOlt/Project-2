4d025673f83a9f6c27d0f8c65736bb011a4edd1b
Chinami Iihama <chinami0420unno@yahoo.co.jp>
2020-04-11 13:09:00 -0700

    added rough timer

--- views/js/timer.js
+++ views/js/timer.js
@@ -1,17 +1,22 @@
-window.onload = function() {
-    var sec = 75;
-    setInterval(function() {
-      var a = new Date();
-      document.getElementById("timer").innerHTML = hou + " : " + sec;
-      sec--;
-      if (sec == 00) {
-        hou--;
-        sec = 60;
-        if (hou == 0) {
-          hou = 2;
-        }
-      }
-    }, 500);
-  };
-  
-  function resetTimer() {}
+var timeEl = document.querySelector(".time");
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 20;
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+  }, 1000);
+}
+
+function sendMessage() {
+  timeEl.textContent = "TIME OVER";
+}
+
+setTime();
