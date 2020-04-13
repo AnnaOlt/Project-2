5497c4a7f9deee5b169066353f0cd20110825a82
millernj <noahjmiller17@gmail.com>
2020-04-13 16:35:11 -0700

    changing routes to controllers / serving views statically

--- server.js
+++ server.js
@@ -9,19 +9,21 @@ var passport = require("./config/passport");
 var PORT = process.env.PORT || 8080;
 var db = require("./models");
 
+const mainController = require("./controllers");
+
 // Creating express app and configuring middleware needed for authentication
 var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("views"));
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Requiring our routes
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+
+app.use('/', mainController);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
