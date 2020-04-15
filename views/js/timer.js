bdee6851b4dc90b3032f65984b26c83600f1cc9b
Michael Myers <anthonymyers1985@gmail.com>
2020-04-15 12:47:40 -0700

    added the style and theme switcher

---
+++ views/js/timer.js
@@ -0,0 +1,22 @@
+var timeEl = document.querySelector(".time");
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 20;
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft;
+
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            sendMessage();
+        }
+    }, 1000);
+}
+
+function sendMessage() {
+    timeEl.textContent = "TIME OVER";
+}
+
+setTime();
